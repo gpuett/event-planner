bafd052713790fa28dcc30055929dfda9b985b13
Garnett Puett <gpuett@gmail.com>
2018-05-25 10:53:53 -0700

    add exception throws for invalid entries

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,18 +15,42 @@ public class App {
                 System.out.println("How many guests do you expect to attend your event?");
                 Integer guests = Integer.parseInt(bufferedReader.readLine());
                 party.setGuestCount(guests);
-                System.out.println("Great! Now choose from our catering option: Pizza, Burgers, or Full Catering");
-                String food = bufferedReader.readLine().toLowerCase();
-                party.setFoodOrdered(food);
-                party.setFoodCost();
+                System.out.println("Now choose from our catering option: Pizza, Burgers, or Full Catering");
+                boolean choosingFood = true;
+                while (choosingFood) {
+                    String food = bufferedReader.readLine().toLowerCase();
+                    if (food.equals("pizza") || food.equals("burgers") || food.equals("full catering")){
+                        party.setFoodOrdered(food);
+                        party.setFoodCost();
+                        choosingFood = false;
+                    } else {
+                        System.out.println("I didn't recognize that entry. Please choose one of the above options.");
+                    }
+                }
                 System.out.println("Yum! What about drinks? Which would you like of the following? Soda, Craft Beer, or Full Bar?");
-                String drink = bufferedReader.readLine().toLowerCase();
-                party.setDrinksOrdered(drink);
-                party.setDrinkCost();
+                boolean choosingdrinks = true;
+                while (choosingdrinks){
+                    String drink = bufferedReader.readLine().toLowerCase();
+                    if (drink.equals("soda") || drink.equals("craft beer") || drink.equals("full bar")){
+                        party.setDrinksOrdered(drink);
+                        party.setDrinkCost();
+                        choosingdrinks = false;
+                    } else {
+                        System.out.println("I didn't recognize that entry. Please choose one of the above options.");
+                    }
+                }
                 System.out.println("Sounds good! What type of musical entertainment would you like? DJ, Band, or none?");
-                String entertainer = bufferedReader.readLine().toLowerCase();
-                party.setEntertainment(entertainer);
-                party.setEntertainmentCost();
+                boolean choosingEntertainment = true;
+                while (choosingEntertainment){
+                    String entertainer = bufferedReader.readLine().toLowerCase();
+                    if (entertainer.equals("dj") || entertainer.equals("band") || entertainer.equals("none")){
+                        party.setEntertainment(entertainer);
+                        party.setEntertainmentCost();
+                        choosingEntertainment = false;
+                    } else {
+                        System.out.println("I didn't recognize that entry. Please choose one of the above options.");
+                    }
+                }
                 party.setEventCost();
                 System.out.println("That will be fun!");
                 System.out.println("The cost of your event will be: $" + party.getEventCost() + ".");
