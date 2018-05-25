0a38a060d916a5a0a0712ee44bae5c8bf0c73bb9
Garnett Puett <gpuett@gmail.com>
2018-05-25 08:11:58 -0700

    test instance of Event class

---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,13 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+    @Test
+    public void newEvent_instantiatesCorrectly_true() throws Exception{
+        Event testEvent = new Event();
+        assertEquals(true, testEvent instanceof Event);
+    }
+}
