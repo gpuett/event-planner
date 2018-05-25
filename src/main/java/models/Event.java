57799d20980967f6200aedcd6969bd277ee61ac1
Garnett Puett <gpuett@gmail.com>
2018-05-25 08:22:45 -0700

    test setter an getter for guest count property

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,4 +1,19 @@
 package models;
 
 public class Event {
+    private int guestCount;
+    private String foodOrdered;
+    private String drinksOrdered;
+    private String entertainment;
+
+    public void setGuestCount(int count) {
+        this.guestCount = count;
+    }
+
+    public int getGuestCount() {
+        return guestCount;
+    }
+
+
+
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EventTest {
+
     @Test
     public void newEvent_instantiatesCorrectly_true() throws Exception{
         Event testEvent = new Event();
         assertEquals(true, testEvent instanceof Event);
+    }
+
+    @Test
+    public void getGuestCount_correctlyGetsGuestCount_integer() {
+        Event testEvent = new Event();
+        testEvent.setGuestCount(50);
+        assertEquals(50, testEvent.getGuestCount());
+
     }
 }
